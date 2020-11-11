8609bdb95142a42ac588e2aeffa4a196a3b98490
Toni Alatalo <toni@playsign.net>
2020-11-11 21:57:23 +0200

    get answer from user, and display the correct answer

--- fractionpractice.js
+++ fractionpractice.js
@@ -3,14 +3,25 @@ function randomInt(max) {
 }
 
 function randomForFraction(max) {
-    return randomInt(max - 1) + 1;
+    return randomInt(max) + 1;
 }
 
 const nomElem = document.getElementById("nom");
 const divElem = document.getElementById("div");
 
-let nom = randomForFraction(10);
-let div = randomForFraction(10);
+const nom = randomForFraction(9);
+const div = randomForFraction(9);
 
 nomElem.innerHTML = nom;
 divElem.innerHTML = div;
+
+const answerElem = document.getElementById("answer");
+const feedbackElem = document.getElementById("feedback");
+const resultElem = document.getElementById("result");
+
+answerElem.onchange = function() {
+    feedbackElem.innerHTML = answerElem.value;
+
+    const correct = nom / div;
+    resultElem.innerHTML = correct;
+};
